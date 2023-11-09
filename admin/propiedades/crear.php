86a30302539f3e5231019f46a034137ae4655fb6
S4vi0r <gbp17@outlook.com>
2023-11-09 14:46:48 -0500

    Las propiedades creadas se guardan en la base de datos, pero no se muestran en la página principal.

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -6,7 +6,38 @@ require '../../includes/config/database.php';
 
 $db = conectarDB();
 
-var_dump($db);
+// var_dump($db);
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    
+    // echo "<pre>";
+
+    // var_dump($_POST);
+
+    // echo "</pre>";
+
+
+    $titulo = $_POST["titulo"];
+    $precio = $_POST["precio"];
+    $descripcion = $_POST["descripcion"];
+    $habitaciones = $_POST["habitaciones"];
+    $wc = $_POST["wc"];
+    $estacionamiento = $_POST["estacionamiento"];
+    $Vendedores_id = $_POST["vendedor"];
+
+    // Insertar en la base de datos
+
+    $query = "INSERT INTO propiedades (titulo, precio, descripcion, habitaciones, wc, estacionamiento, Vendedores_id) VALUES ('$titulo', '$precio', '$descripcion', '$habitaciones', '$wc', '$estacionamiento', '$Vendedores_id')";
+
+    // echo $query;
+
+    $resultado = mysqli_query($db, $query);
+
+    if ($resultado) {
+        echo "Insertado correctamente";
+    }
+}
+
 
 require '../../includes/functions.php';
 
@@ -17,18 +48,18 @@ addTemplate('header');
 
     <a href="/Real-Estate/admin/index.php" class="btn btn-green">Volver</a>
 
-    <form action="" class="form">
+    <form method="POST" action="/Real-Estate/admin/propiedades/crear.php" class="form">
         <fieldset>
             <legend>Informacion General</legend>
 
             <label for="titulo">Titulo:</label>
             <input type="text" name="titulo" id="titulo" placeholder="Titulo de la propiedad">
 
-            <label for="precio">Titulo:</label>
+            <label for="precio">Precio:</label>
             <input type="number" name="precio" id="precio" placeholder="Precio de la propiedad">
 
             <label for="imagen">Imagen:</label>
-            <input type="file" name="imagen" id="imagen" accept="image/jpeg, image/png">
+            <input type="file" id="imagen" accept="image/jpeg, image/png">
 
             <label for="descripcion">Descripcion:</label>
             <textarea name="descripcion" id="descripcion" cols="30" rows="10"></textarea>
@@ -38,17 +69,17 @@ addTemplate('header');
 
             <label for="habitaciones">Habitaciones:</label>
             <input type="number" name="habitaciones" id="habitaciones" placeholder="Eg. 3" min="1" max="9">
-            
+
             <label for="wc">Baños:</label>
             <input type="number" name="wc" id="wc" placeholder="Eg. 4" min="1" max="9">
-            
+
             <label for="estacionamiento">Estacionamiento:</label>
             <input type="estacionamiento" name="estacionamiento" id="estacionamiento" placeholder="Eg. 2" min="1" max="9">
         </fieldset>
         <fieldset>
             <legend>Vendedor</legend>
 
-            <select name="" id="">
+            <select name="vendedor" id="">
                 <option value="1">Nacho</option>
                 <option value="2">Viviana</option>
             </select>
